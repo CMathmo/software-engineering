e626acadc2fd9062c79bcd2e9c05bec6ad0a9c79
haolin <970253303@qq.com>
2020-10-27 21:34:51 +0800

    图表更新
    1.分析界面增加了自定义时间功能
    2.将图表类型菜单变更为下拉菜单
    3.完成了秋阳要求的账户图表

--- t_Book/app/src/main/java/com/wad/tBook/statistical/AccountDetailActivity.kt
+++ t_Book/app/src/main/java/com/wad/tBook/statistical/AccountDetailActivity.kt
@@ -5,12 +5,20 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import com.aachartmodel.aainfographics.AAInfographicsLib.AAChartCreator.AAChartModel
+import com.aachartmodel.aainfographics.AAInfographicsLib.AAChartCreator.AAChartType
+import com.aachartmodel.aainfographics.AAInfographicsLib.AAChartCreator.AAChartView
+import com.aachartmodel.aainfographics.AAInfographicsLib.AAChartCreator.AASeriesElement
 import com.wad.tBook.R
 import com.wad.tBook.room.tBookDatabase
 
 class AccountDetailActivity : AppCompatActivity() {
 
     var AccountDetail: String ?= null
+
+    //图表视图
+    lateinit var AccDetailChartView :AAChartView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_account_detail)
@@ -18,6 +26,9 @@ class AccountDetailActivity : AppCompatActivity() {
         AccountDetail = intent.getStringExtra("account_class")
         AccountClassDetail.text = AccountDetail
         val typeList:List<AccountRepository.TypeAmount> = dataInfo()
+
+        //获取图表控件
+        AccDetailChartView = findViewById(R.id.AccDetailedChartView)
         println(typeList)
         dataDisplay(typeList)
     }
@@ -55,5 +66,26 @@ class AccountDetailActivity : AppCompatActivity() {
         incomeView.text = "收入：" + typeList[0].Amount.toString()
         outcomeView.text = "支出：" + typeList[1].Amount.toString()
         transferView.text = "转账：" + typeList[2].Amount.toString()
+
+        //饼图模型
+        val aaPieModel : AAChartModel = AAChartModel()
+            .chartType(AAChartType.Pie)
+//            .title("title")
+            .dataLabelsEnabled(true)
+            .legendEnabled(false)
+            .series(arrayOf(
+                AASeriesElement()
+                    .name("")
+                    .data(
+                        arrayOf(
+                            setOf("收入", typeList[0].Amount),
+                            setOf("支出", typeList[1].Amount),
+                            setOf("转账", typeList[2].Amount)
+                        )
+
+                    )
+            ))
+        //显示图表
+        AccDetailChartView.aa_drawChartWithChartModel(aaPieModel)
     }
 }
